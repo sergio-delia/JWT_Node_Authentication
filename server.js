07949fbaaf0e02cbe07dcbf66d8cf06c937e2573
Sergio <sergio.delia@yahoo.it>
2023-11-11 23:22:34 +0100

    end of project

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username:'Kyle',
+        title: 'Post 1'
+    },
+    {
+        username:'Jim',
+        title: 'Post 2'
+    }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+})
+
+
+
+
+
+function authenticateToken(req, res, next){
+    
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    /* Perchè nel get ci sarà un authorization: Bearer <il token creato> quindi con split prendiamo la parte dopo lo spazio tra Bearer e il token */
+    if(token == null){
+        return res.sendStatus(403)
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err){
+            return res.sendStatus(403)
+        }
+        req.user = user
+        next()
+    })
+}
+
+app.listen(3000)    
